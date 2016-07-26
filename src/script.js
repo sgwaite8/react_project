47e29cb3422655e017a3548be36d45e7a9fd7a81
Christian Mansour <c.mansour@miami.edu>
2016-07-26 11:48:16 -0500

    set up all of the components, everything working perfectly

--- src/script.js
+++ src/script.js
@@ -1,7 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import SomeComponent from './components/some_component';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TaskContainer from './components/task-container';
 
-// ReactDOM.render(<SomeComponent />, document.getElementById('some-id'));
-
-console.log('herro');
+ReactDOM.render(<TaskContainer />, document.getElementById('task-container'));
